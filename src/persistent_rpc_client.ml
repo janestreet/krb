0109aacfec72c6b4b440457078fc9fbf03cf8644
public-release <as-public-release@janestreet.com>
2023-04-04 11:04:59 +0100

    v0.16~preview.128.14+51

--- src/persistent_rpc_client.ml
+++ src/persistent_rpc_client.ml
@@ -1,48 +1,65 @@
 open Core
 open Async
-include Persistent_connection.Rpc
 
-type 'a persistent_connection_args =
-  server_name:string
-  -> ?log:Log.t
-  -> ?on_event:(Host_and_port.t Event.t -> unit Deferred.t)
-  -> ?retry_delay:(unit -> Time_float.Span.t)
-  -> 'a
+module Create_helper (T : sig
+    include Persistent_connection.S
 
-(* shadow included [create'] *)
-let create'
-      ~server_name
-      ?log
-      ?on_event
-      ?retry_delay
-      ?max_message_size
-      ?handshake_timeout
-      ?heartbeat_config
-      ~krb_mode
-      ?bind_to_address
-      ?implementations
-      ?description
-      ?cred_cache
-      ~authorize
-      get_addr
-  =
-  create
-    ~server_name
-    ?log
-    ?on_event
-    ?retry_delay
-    ~connect:(fun host_and_port ->
-      let host, port = Host_and_port.tuple host_and_port in
-      Kerberized_rpc.Connection.client
+    val conn_of_rpc_connection
+      :  Kerberized_rpc.Connection.t Deferred.Or_error.t
+      -> conn Deferred.Or_error.t
+  end) =
+struct
+  include T
+
+  let create'
+        ~server_name
+        ?log
+        ?on_event
+        ?retry_delay
         ?max_message_size
         ?handshake_timeout
         ?heartbeat_config
+        ~krb_mode
+        ?bind_to_address
         ?implementations
         ?description
         ?cred_cache
         ~authorize
-        ~krb_mode
-        (Tcp.Where_to_connect.of_host_and_port ?bind_to_address { host; port }))
-    ~address:(module Host_and_port)
-    get_addr
-;;
+        get_addr
+    =
+    T.create
+      ~server_name
+      ?log
+      ?on_event
+      ?retry_delay
+      ~connect:(fun host_and_port ->
+        let host, port = Host_and_port.tuple host_and_port in
+        Kerberized_rpc.Connection.client
+          ?max_message_size
+          ?handshake_timeout
+          ?heartbeat_config
+          ?implementations
+          ?description
+          ?cred_cache
+          ~authorize
+          ~krb_mode
+          (Tcp.Where_to_connect.of_host_and_port ?bind_to_address { host; port })
+        |> T.conn_of_rpc_connection)
+      ~address:(module Host_and_port)
+      get_addr
+  ;;
+end
+
+module Rpc = Create_helper (struct
+    include Persistent_connection.Rpc
+
+    let conn_of_rpc_connection = Fn.id
+  end)
+
+module Versioned_rpc = Create_helper (struct
+    include Persistent_connection.Versioned_rpc
+
+    let conn_of_rpc_connection =
+      Deferred.Or_error.bind ~f:Versioned_rpc.Connection_with_menu.create
+    ;;
+  end)
--- src/persistent_rpc_client.mli
+++ src/persistent_rpc_client.mli
@@ -1,28 +1,43 @@
 open! Core
 open! Async
 
-(** An instantiation of [Persistent_connection] for creating persistent kerberized rpc
-    connections *)
-
-include Persistent_connection.S with type conn = Rpc.Connection.t
+(** An instantiation of [Persistent_connection] for creating persistent kerberized rpc and
+    versioned_rpc connections *)
 
 (** Arguments passed through to [Persistent_connection.Rpc.create].
     See [Persistent_connection] for documentation. *)
-type 'a persistent_connection_args =
+type ('a, 'event) persistent_connection_args :=
   server_name:string
   -> ?log:Log.t
-  -> ?on_event:(Host_and_port.t Event.t -> unit Deferred.t)
+  -> ?on_event:('event -> unit Deferred.t)
   -> ?retry_delay:(unit -> Time_float.Span.t)
   -> 'a
 
-val create'
-  : (krb_mode:Mode.Client.t
+type ('event, 't, 'state) create :=
+  ( (krb_mode:Mode.Client.t
      -> ?bind_to_address:Unix.Inet_addr.t
-     -> ?implementations:(Server_principal.t -> _ Rpc.Connection.Client_implementations.t)
+     -> ?implementations:
+       (Server_principal.t -> 'state Rpc.Connection.Client_implementations.t)
      -> ?description:Info.t
      -> ?cred_cache:Cred_cache.t
      -> authorize:Authorize.t
      -> (unit -> Host_and_port.t Deferred.Or_error.t)
-     -> t)
+     -> 't)
       Kerberized_rpc.async_rpc_args
-      persistent_connection_args
+  , 'event )
+    persistent_connection_args
+
+(** Arguments passed through to [Persistent_connection.Rpc.create].
+    See [Persistent_connection] for documentation. *)
+
+module Versioned_rpc : sig
+  include Persistent_connection.S with type conn = Versioned_rpc.Connection_with_menu.t
+
+  val create' : (Host_and_port.t Event.t, t, 'a) create
+end
+
+module Rpc : sig
+  include Persistent_connection.S with type conn = Rpc.Connection.t
+
+  val create' : (Host_and_port.t Event.t, t, 'a) create
+end
